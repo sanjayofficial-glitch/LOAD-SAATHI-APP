5aa03a270b3faeb72bd69b94ecf4f94a0170c1f8
[dyad] <sanjayofficial720@gmail.com>
2026-03-28 10:30:34 +0530

    [dyad] wrote 3 file(s)

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,5 +1,9 @@
 "use client";
 
 import * as React from "react";
-import { Chart as ChartPrimitive } from "./chart";
-const Chart = ({ ...props })=>{return <ChartPrimitive {...props}/>};export default Chart;
+
+function Chart({ } : { } ) {
+  return null;
+}
+
+export default Chart;
--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,6 +1,4 @@
 "use client";
 
-import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
-var _s = $RefreshSig$();
-import { Toaster as Sonner } from "sonner";
-const Toaster = ({ ...props })=>{return <Toaster {...props}/>};export default Toaster;
+import { Toaster } from "sonner";
+export { Toaster };
