a9851303d7dd0fd7cc85d79234b2a25a6add9114
[dyad] <sanjayofficial720@gmail.com>
2026-03-31 17:49:37 +0530

    [dyad] Completing main.tsx setup - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./globals.css";
 import App from "./App";
 import ClerkProviderWrapper from "./components/ClerkProvider";
+import { AuthProvider } from "./contexts/AuthContext";
+import { Toaster } from "sonner";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProviderWrapper>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+          <Toaster />
+        </AuthProvider>
+      </QueryClientProvider>
     </ClerkProviderWrapper>
   </StrictMode>,
 );
