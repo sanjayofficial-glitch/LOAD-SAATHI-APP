bd45b8044ab30c271fa6b2f472720517e7b94701
[dyad] <sanjayofficial720@gmail.com>
2026-04-01 23:09:50 +0530

    [dyad] Updating React Router future flags - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ if (!publishableKey) {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={publishableKey}>
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <App />
       </BrowserRouter>
     </ClerkProvider>
