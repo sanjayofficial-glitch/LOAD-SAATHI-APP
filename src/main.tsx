cd34a80b7c91d27ae88d5ca8439aefd8e2cfc6dd
[dyad] <sanjayofficial720@gmail.com>
2026-04-01 20:18:40 +0530

    [dyad] Fixing CSS import in main.tsx - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
-import "./index.css";
+import "./globals.css";
 
 const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
