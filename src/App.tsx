a88d612bdc67da51ae9aae62f6622fccfdf48324
[dyad] <sanjayofficial720@gmail.com>
2026-04-09 15:27:31 +0530

    [dyad] Completing App.tsx with missing routes - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
-import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-react";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { ClerkProvider, SignedIn } from "@clerk/clerk-react";
 import { AuthProvider } from "./contexts/AuthContext";
 import Layout from "./components/Layout";
 import Index from "./pages/Index";
@@ -12,8 +12,11 @@ import TruckerDashboard from "./pages/trucker/Dashboard";
 import ShipperDashboard from "./pages/shipper/Dashboard";
 import PostTrip from "./pages/trucker/PostTrip";
 import MyTrips from "./pages/trucker/MyTrips";
+import EditTrip from "./pages/trucker/EditTrip";
 import PostShipments from "./pages/shipper/PostShipments";
 import MyShipments from "./pages/shipper/MyShipments";
+import EditShipment from "./pages/shipper/EditShipment";
+import ShipmentDetail from "./pages/shipper/ShipmentDetail";
 import BrowseTrips from "./pages/shipper/BrowseTrips";
 import BrowseShipments from "./pages/trucker/BrowseShipments";
 import TripDetail from "./pages/TripDetail";
@@ -46,12 +49,15 @@ const App = () => {
               <Route path="/trucker/dashboard" element={<RoleProtectedRoute allowedRole="trucker"><TruckerDashboard /></RoleProtectedRoute>} />
               <Route path="/trucker/post-trip" element={<RoleProtectedRoute allowedRole="trucker"><PostTrip /></RoleProtectedRoute>} />
               <Route path="/trucker/my-trips" element={<RoleProtectedRoute allowedRole="trucker"><MyTrips /></RoleProtectedRoute>} />
+              <Route path="/trucker/trips/:tripId/edit" element={<RoleProtectedRoute allowedRole="trucker"><EditTrip /></RoleProtectedRoute>} />
               <Route path="/trucker/browse-shipments" element={<RoleProtectedRoute allowedRole="trucker"><BrowseShipments /></RoleProtectedRoute>} />
               
               {/* Shipper Routes */}
               <Route path="/shipper/dashboard" element={<RoleProtectedRoute allowedRole="shipper"><ShipperDashboard /></RoleProtectedRoute>} />
               <Route path="/shipper/post-shipment" element={<RoleProtectedRoute allowedRole="shipper"><PostShipments /></RoleProtectedRoute>} />
               <Route path="/shipper/my-shipments" element={<RoleProtectedRoute allowedRole="shipper"><MyShipments /></RoleProtectedRoute>} />
+              <Route path="/shipper/shipments/:id" element={<RoleProtectedRoute allowedRole="shipper"><ShipmentDetail /></RoleProtectedRoute>} />
+              <Route path="/shipper/shipments/:shipmentId/edit" element={<RoleProtectedRoute allowedRole="shipper"><EditShipment /></RoleProtectedRoute>} />
               <Route path="/browse-trucks" element={<RoleProtectedRoute allowedRole="shipper"><BrowseTrips /></RoleProtectedRoute>} />
               
               {/* Common Protected Routes */}
