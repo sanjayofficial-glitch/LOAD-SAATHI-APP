ee727ba9cd0de4701b0ee10197963d79e50107bc
[dyad] <sanjayofficial720@gmail.com>
2026-04-27 14:45:33 +0530

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -30,51 +30,49 @@ import AdminMonitoring from "./pages/admin/MonitoringDashboard";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/choose-role" element={<ChooseRole />} />
-        <Route path="/auth-sync" element={<AuthSync />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/chat/:requestId" element={<Chat />} />
-        <Route path="/messages" element={<ChatList />} />
-        <Route path="/trips/:tripId" element={<TripDetail />} />
-        <Route path="/shipments/:id" element={<ShipmentDetail />} />
-        <Route path="/shipments/:shipmentId/edit" element={<EditShipment />} />
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/choose-role" element={<ChooseRole />} />
+      <Route path="/auth-sync" element={<AuthSync />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/chat/:requestId" element={<Chat />} />
+      <Route path="/messages" element={<ChatList />} />
+      <Route path="/trips/:tripId" element={<TripDetail />} />
+      <Route path="/shipments/:id" element={<ShipmentDetail />} />
+      <Route path="/shipments/:shipmentId/edit" element={<EditShipment />} />
 
-        {/* Shipper routes */}
-        <Route element={<RoleProtectedRoute allowedRole="shipper" />}>
-          <Route path="/shipper/dashboard" element={<ShipperDashboard />} />
-          <Route path="/shipper/my-shipments" element={<ShipperHistory />} />
-          <Route path="/shipper/shipments/:id" element={<ShipmentDetail />} />
-          <Route path="/shipper/shipments/:shipmentId/edit" element={<EditShipment />} />
-          <Route path="/shipper/history" element={<ShipperHistory />} />
-          <Route path="/browse-trucks" element={<BrowseTrips />} />
-          <Route path="/shipper/post-shipment" element={<PostShipments />} />
-        </Route>
+      {/* Shipper routes */}
+      <Route element={<RoleProtectedRoute allowedRole="shipper" />}>
+        <Route path="/shipper/dashboard" element={<ShipperDashboard />} />
+        <Route path="/shipper/my-shipments" element={<ShipperHistory />} />
+        <Route path="/shipper/shipments/:id" element={<ShipmentDetail />} />
+        <Route path="/shipper/shipments/:shipmentId/edit" element={<EditShipment />} />
+        <Route path="/shipper/history" element={<ShipperHistory />} />
+        <Route path="/browse-trucks" element={<BrowseTrips />} />
+        <Route path="/shipper/post-shipment" element={<PostShipments />} />
+      </Route>
 
-        {/* Trucker routes */}
-        <Route element={<RoleProtectedRoute allowedRole="trucker" />}>
-          <Route path="/trucker/dashboard" element={<TruckerDashboard />} />
-          <Route path="/trucker/my-trips" element={<TruckerHub />} />
-          <Route path="/trucker/trips/:tripId" element={<TruckerTripDetail />} />
-          <Route path="/trucker/trips/:tripId/edit" element={<EditTrip />} />
-          <Route path="/trucker/browse-shipments" element={<BrowseShipments />} />
-          <Route path="/trucker/post-trip" element={<PostTrip />} />
-          <Route path="/trucker/history" element={<TruckerHistory />} />
-        </Route>
+      {/* Trucker routes */}
+      <Route element={<RoleProtectedRoute allowedRole="trucker" />}>
+        <Route path="/trucker/dashboard" element={<TruckerDashboard />} />
+        <Route path="/trucker/my-trips" element={<TruckerHub />} />
+        <Route path="/trucker/trips/:tripId" element={<TruckerTripDetail />} />
+        <Route path="/trucker/trips/:tripId/edit" element={<EditTrip />} />
+        <Route path="/trucker/browse-shipments" element={<BrowseShipments />} />
+        <Route path="/trucker/post-trip" element={<PostTrip />} />
+        <Route path="/trucker/history" element={<TruckerHistory />} />
+      </Route>
 
-        {/* Admin route */}
-        <Route element={<RoleProtectedRoute allowedRole="admin" />}>
-          <Route path="/admin/monitoring" element={<AdminMonitoring />} />
-        </Route>
+      {/* Admin route */}
+      <Route element={<RoleProtectedRoute allowedRole="admin" />}>
+        <Route path="/admin/monitoring" element={<AdminMonitoring />} />
+      </Route>
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
