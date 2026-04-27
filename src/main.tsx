74b7e162562d865fdc08f2f56199fc7c2de315a0
[dyad] <sanjayofficial720@gmail.com>
2026-04-27 14:01:53 +0530

    [dyad] wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./globals.css";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+rootElement.innerHTML = "";
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
